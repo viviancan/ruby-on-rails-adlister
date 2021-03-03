6dc65731fcecc949e24bba02a891754d91109060
viviancan <vcnales@gmail.com>
2021-03-02 20:24:19 -0600

    iniitial commit

---
+++ db/migrate/20210302225117_create_ads.rb
@@ -0,0 +1,10 @@
+class CreateAds < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ads do |t|
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210303021111_add_user_id_to_ads.rb
@@ -0,0 +1,6 @@
+class AddUserIdToAds < ActiveRecord::Migration[6.1]
+  def change
+    add_column :ads, :user_id, :integer
+    add_index :ads, :user_id
+  end
+end
