8c8ec4c9212197ecd8915c870fa65f091d873415
viviancan <vcnales@gmail.com>
2021-03-03 13:26:19 -0600

    updated version to 6.0 for migration

--- db/migrate/20210302225117_create_ads.rb
+++ db/migrate/20210302225117_create_ads.rb
@@ -1,4 +1,4 @@
-class CreateAds < ActiveRecord::Migration[6.1]
+class CreateAds < ActiveRecord::Migration[6.0]
   def change
     create_table :ads do |t|
       t.string :title
--- db/migrate/20210303021111_add_user_id_to_ads.rb
+++ db/migrate/20210303021111_add_user_id_to_ads.rb
@@ -1,4 +1,4 @@
-class AddUserIdToAds < ActiveRecord::Migration[6.1]
+class AddUserIdToAds < ActiveRecord::Migration[6.0]
   def change
     add_column :ads, :user_id, :integer
     add_index :ads, :user_id
